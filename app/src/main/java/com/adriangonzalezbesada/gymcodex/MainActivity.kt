9f01aa343e99cfd65ad3c72b3d9e1b372e5b64fb
AdrianGonzalezBesada <adrian.gonzalez.besada@fpfomento.com>
2025-01-26 17:53:09 +0100

    Pruebas e importaciones de dependencias necesarias

--- app/src/main/java/com/adriangonzalezbesada/gymcodex/MainActivity.kt
+++ app/src/main/java/com/adriangonzalezbesada/gymcodex/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.adriangonzalezbesada.gymcodex.ui.theme.GymCodexTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +28,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             GymCodexTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                    Column() {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        HelloScreen(
+
+                        )
+                    }
                 }
             }
         }
@@ -37,6 +51,31 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+@Composable
+fun HelloScreen() {
+    var name by rememberSaveable { mutableStateOf("") }
+    HelloContent(name = name, onNameChange = { name = it })
+
+}
+@Composable
+fun HelloContent(name: String, onNameChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text(
+            text = "Hello, $name",
+            modifier = Modifier.padding(bottom = 8.dp),
+            style = MaterialTheme.typography.headlineSmall
+        )
+        OutlinedTextField(
+            value = name,
+            onValueChange = onNameChange,
+            label = { Text("Name") }
+        )
+    }
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
